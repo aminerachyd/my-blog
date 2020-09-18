88dfa12736bea3a4eeacfb0ec377e5ac7eb4bd1b
Amine Rachyd <aminerachyd99@gmail.com>
2020-09-18 12:40:32 +0100

    First test of deployment
    Build react app -> move build directory to root -> firebase deploy
    Need to implement portfolio in backend.

---
+++ build/precache-manifest.eb9f6fe03bcab79a68403db93989b99b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fef9f66f59effa86b3f007ae94a7f761",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8500548bb5cc8cead60",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c8a9aa2d8581f239d0dc",
+    "url": "/static/css/main.99a7c9b9.chunk.css"
+  },
+  {
+    "revision": "e8500548bb5cc8cead60",
+    "url": "/static/js/2.951c27ef.chunk.js"
+  },
+  {
+    "revision": "320addaff3e37295a01ab28fa4b66477",
+    "url": "/static/js/2.951c27ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8a9aa2d8581f239d0dc",
+    "url": "/static/js/main.f750f9bc.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  },
+  {
+    "revision": "737da7881179ff3fccec8963907fd8ea",
+    "url": "/static/media/blog-name-2.737da788.jpg"
+  }
+]);
