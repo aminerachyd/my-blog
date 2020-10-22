1266952f20e4353fa63cd376234f844d65f1f0a1
Amine Rachyd <aminerachyd99@gmail.com>
2020-10-22 23:37:57 +0100

    Mingling stuff with slate, started the walkthrough

--- client/src/components/about/aboutme/AboutMe.js
+++ client/src/components/about/aboutme/AboutMe.js
@@ -1,11 +1,15 @@
 import React, { Fragment, useEffect } from "react";
 import AboutParagraphs from "./AboutParagraphs";
+import RichTextEditor from "../../richtexteditor/RichTextEditor";
 
 const AboutMe = () => {
   return (
     <Fragment>
       <h2 className="display-4">About Me</h2>
       <hr />
+
+      {/* FIXME for testing purposes */}
+      <RichTextEditor />
 
       <div className="card my-3 blog-post">
         <div className="container card-body p-5">
